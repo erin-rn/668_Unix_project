fee2db926dcaefe2d2a6c11c04cd11d47ac605af
erin-rn <157153134+erin-rn@users.noreply.github.com>
2024-03-05 11:08:48 -0800

    Add files via upload
    Correct project file

--- Horkan_project_bash.sh
+++ Horkan_project_bash.sh
@@ -6,21 +6,23 @@ echo "It works!"
 
 
 ### (1) In your home directory make a directory called RAW_DATA
-mkdir ~/RAW_DATA
+mkdir $HOME/RAW_DATA
 ### (2) Copy all .fna fasta files  
 ###     from home directory into RAW_DATA (must work from any directory.)
-cp file == .fna 
+cp $HOME/*.fna $HOME/RAW_DATA
 ### (3) Do the same with all primer files ending with .csv
-
+cp $HOME/*.csv $HOME/RAW_DATA/
 ### (4) In your home directory, make 2 directories: P_DATA and RESULTS
-mkdir ~/P_DATA ~/RESULTS
+mkdir $HOME/P_DATA $HOME/RESULTS
 ### (5) Add all three directories to your $PATH: ~/RAW_DATA, ~/P_DATA
 ###     and ~/RESULTS
-
+PATH =$HOME/RAW_DATA:$HOME/P_DATA:$HOME/RESULTS:$PATH
 ### (6) Write your entire $PATH, the text string RAW_DATA and the 
 ###    contents of this directory (the names of the files) into a new file 
 ###    called 'readme.txt' in your home directory
-echo 'RAW_DATA' > readme.txt
+echo $PATH > readme.txt
+echo 'RAW_DATA' >> readme.txt
+ls * >> readme.txt
 ### (7) The last command should output the contents of readme.txt
 ###     to the terminal.
 cat readme.txt
